c0a687e02d3470e3d91e5aa1b5401be8a706287b
lingengyue <1090899684@qq.com>
2022-04-23 23:51:15 +0800

    docker

---
+++ designpattern/src/main/java/InterfaceSegregationDemo.java
@@ -0,0 +1,70 @@
+/**
+ * 接口隔离原则
+ * 类实现接口时，应依赖于最少的类，避免实现不需要的接口
+ * 通过切分接口类进行隔离
+ */
+public class InterfaceSegregationDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+interface Interface1 {
+    void run1();
+    void run2();
+}
+
+interface Interface2 {
+    void run3();
+    void run4();
+}
+
+class A implements Interface1 {
+
+    @Override
+    public void run1() {
+
+    }
+
+    @Override
+    public void run2() {
+
+    }
+}
+
+class B implements Interface2 {
+
+    @Override
+    public void run3() {
+
+    }
+
+    @Override
+    public void run4() {
+
+    }
+}
+
+class C implements Interface1, Interface2 {
+
+    @Override
+    public void run1() {
+
+    }
+
+    @Override
+    public void run2() {
+
+    }
+
+    @Override
+    public void run3() {
+
+    }
+
+    @Override
+    public void run4() {
+
+    }
+}
---
+++ designpattern/src/main/java/SingleResponsibilityDemo.java
@@ -0,0 +1,27 @@
+/**
+ * 单一职责原则
+ */
+public class SingleResponsibilityDemo {
+
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.run("汽车");
+        vehicle.fly("飞机");
+        vehicle.sail("轮船");
+    }
+}
+
+class Vehicle {
+    public void run(String name) {
+        System.out.println(name + "正在公路上行驶");
+    }
+
+    public void fly(String name) {
+        System.out.println(name + "正在空中飞行");
+    }
+
+    public void sail(String name) {
+        System.out.println(name + "正在水中行驶");
+    }
+
+}
